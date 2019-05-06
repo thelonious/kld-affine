59dbe2eb169b788d9aa660077419430347b7f7b1
Kevin Lindsey <kevin@kevlindev.com>
2019-05-06 14:26:31 -0700

    Add JSDoc for namespaces and modules

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+/**
+ *  @module kld-affine
+ */
+
 // expose classes
 
+/**
+ *  @namespace Point2D
+ *  @implements {module:Point2D~Point2D}
+ */
 export {default as Point2D} from "./lib/Point2D.js";
+
+/**
+ *  @namespace Vector2D
+ *  @implements {module:Vector2D~Vector2D}
+ */
 export {default as Vector2D} from "./lib/Vector2D.js";
+
+/**
+ *  @namespace Matrix2D
+ *  @implements {module:Matrix2D~Matrix2D}
+ */
 export {default as Matrix2D} from "./lib/Matrix2D.js";
